2ed30324746c7a3cc9f910b73d8971ba0c11acfe
Victor Gutierrez Diban <vgutierrezdiban@protonmail.com>
2020-10-06 00:42:29 -0300

    Work on indentation and readability

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :tweets, dependent: :destroy
-  has_many :likes, dependent: :destroy
+  has_many :tweets,  dependent: :destroy
+  has_many :likes,   dependent: :destroy
   has_many :friends, dependent: :destroy
 
   def to_s
